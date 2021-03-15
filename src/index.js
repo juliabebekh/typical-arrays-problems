a3b55834b258020220c0d429c402c00e32561155
Julia Bebekh <juliabebekh@gmail.com>
2021-03-15 22:32:02 +0300

    init: start the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0){
+    return 0;
+  } else {
+    let number = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < number) {
+        number = array[i]
+      }
+    } 
+    return number
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0){
+    return 0;
+  } else {
+    let number = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > number) {
+        number = array[i]
+      }
+    } 
+    return number
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0){
+    return 0;
+  } else {
+    let summ = 0;
+    for (let i = 0; i < array.length; i++) {
+    summ += array[i]
+    }
+    let result = summ / array.length;
+    return result
+  }
 }
